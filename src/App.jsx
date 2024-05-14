c44f4b4ab37b878923d2dcc7477ce9555f6305d5
freab <fremesfin1@gmail.com>
2024-05-14 21:30:55 +0300

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Loader } from "@react-three/drei";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 12], fov: 45 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <Suspense fallback={<Loader />}>
+      <Canvas shadows camera={{ position: [0, 0, 12], fov: 45 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas>
+    </Suspense>
   );
 }
 
